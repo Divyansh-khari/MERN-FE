2ea0e9d3c1480d8d3afaf92ba4c0813c38af36f1
Divyansh-khari <dannykhari@gmail.com>
2023-09-04 15:11:46 -0700

    Ready for deploy

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'https://divyansh-khari-mern.onrender.com',
+    baseUrl: 'https://divyansh-khari-mern-be.onrender.com/',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
