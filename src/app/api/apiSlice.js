989728b29289631f1d721f999db55224b84214f2
Divyansh-khari <dannykhari@gmail.com>
2023-09-04 22:48:24 -0700

    fix bugs

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -3,6 +3,7 @@ import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
     baseUrl: 'https://divyansh-khari-mern-be.onrender.com/',
+    mode: 'no-cors',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
